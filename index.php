d1152ea614963303e5e3176ea019389f26fe71c4
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2019-04-28 07:28:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Rain Animation</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body> 
+	<div class="rain">
+		<div class="robot"><img src="robot-edit">
+			<div class="text">
+			<h2>Error 404</h2>
+			<h4>Page Not Found</h4>
+			<p></p>
+			
+		</div>
+			
+		<div>
+	
+	</div>
+</body>
+</html>
