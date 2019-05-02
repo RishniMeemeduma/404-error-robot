ead3360c38890ebe5e0d79cb6072bb0c9963c1a5
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2019-05-02 21:41:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
+
+<meta charset="UTF-8">
+  <meta name="description" content="Robot 404 Page">
+  <meta name="keywords" content="HTML,CSS,404,Not found">
+  <meta name="author" content="Rishni Meemeduma">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Rain Animation</title>
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body> 
 	<div class="rain">
-		<div class="robot"><img src="robot-edit.png">
+		<!-- 404 Text begin -->
 			<div class="text">
 			<h2>Error 404</h2>
 			<h4>Page Not Found</h4>
-			<p></p>
+			</div>
+		<!-- 404 Text End-->	
 			
-		</div>
-			
-		<div>
+	<div>
 	
-	</div>
 </body>
 </html>
